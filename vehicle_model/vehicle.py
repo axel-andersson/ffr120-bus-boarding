4b7ce63c5e2cde353355facf1884f349c18b4225
Axel Andersson <axel.andersson@vyrga.com>
2025-12-03 15:55:39 +0100

    added vehicle walls init

--- vehicle_model/vehicle.py
+++ vehicle_model/vehicle.py
@@ -19,10 +19,51 @@ class VehicleWalls:
     """
 
     def __init__(self, points):
-        pass
 
-    def draw(ax: plt.Axes):
-        pass
+        if len(points) < 4:
+            raise "Need at least 4 points for walls with right angles."
+
+        segments = []
+        for i in range(len(points) - 1):
+            start_point = points[i]
+            end_point = points[i + 1]
+
+            dx = end_point[0] - start_point[0]
+            dy = end_point[1] - start_point[1]
+
+            if dx == 0 and dy == 0:
+                raise "Invalid geometry, points cannot overlap"
+            if dx != 0 and dy != 0:
+                raise "Invalid geometry, only vertical and horizontal segments are allowed"
+
+            segment = [start_point, end_point]
+            segments.append(segment)
+
+        start_point = points[-1]
+        end_point = points[0]
+
+        dx = end_point[0] - start_point[0]
+        dy = end_point[1] - start_point[1]
+
+        if dx == 0 and dy == 0:
+            raise "Invalid geometry, points cannot overlap"
+        if dx != 0 and dy != 0:
+            raise "Invalid geometry, only vertical and horizontal segments are allowed"
+
+        segment = [start_point, end_point]
+        segments.append(segment)
+
+        self.wall_segments = segments
+        self.door_segments = []
+
+    def draw(self, ax: plt.Axes):
+        for segment in self.wall_segments:
+            print(segment)
+            x0 = segment[0][0]
+            x1 = segment[1][0]
+            y0 = segment[0][1]
+            y1 = segment[1][1]
+            ax.plot([x0, x1], [y0, y1], color="#a0a0a0", lw=3)
 
 
 class VehicleDoors:
@@ -30,7 +71,7 @@ class VehicleDoors:
     Helper class for doors for vehicle
     """
 
-    def __init__(self, point, width):
+    def __init__(self, name, position, width):
         pass
 
     def draw(ax: plt.Axes):
@@ -95,3 +136,17 @@ class GlobalNode:
 # Things that need to be solved:
 # Rectangle merging
 # Auto waypoint-categorization.
+
+
+#
+# Dummy code for testing below:
+#
+
+
+vw = VehicleWalls([[0, 0], [10, 0], [10, 5], [0, 5]])
+print(vw.wall_segments)
+
+ax = plt.gca()
+ax.set_aspect("equal")
+vw.draw(ax)
+plt.show()
