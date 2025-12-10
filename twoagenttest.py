e2f088dc9a9c2eaff571df07e25bd19f4bb14547
Edith Kadefors <edithkad@gmail.com>
2025-12-10 10:23:28 +0100

    changes waiting time to seconds and a ticket checking disk

--- twoagenttest.py
+++ twoagenttest.py
@@ -11,74 +11,66 @@ canvas = Canvas(tk, background='#ECECEC')
 tk.attributes('-topmost', 0)
 canvas.place(x=10, y=10, height=window_size, width=window_size)
 
-# --- inga väggar i detta test ---
-
+# --- väggar
 walls = np.array([
-    [[0.0, 0.0], [10.0, 0.0]],  
-    [[0.0, 2.0], [10.0, 2.0]], 
-   
+    [[0.0, 0.0], [0.0, 0.0]]
+    #[[0.0, 0.0], [10.0, 0.0]],  
+    #[[0.0, 2.0], [10.0, 2.0]], 
 ])
 
 
-#walls = np.zeros((0, 2, 2))
-
-# --- två agenter mitt emot varandra ---
+# --- agents
 agent1 = MovementAgent(
-    x=-1.0,
-    y=1.2,
-    angle=0.0,             
+    x=0.0,
+    y=-2.0,
+    angle=0.0,
     radius=0.3,
     epsilon=0.1,
     box_length=3,
     box_width=1.5,
-    target=np.array([12.0, 1]),
     dt=0.1
 )
 
 agent2 = MovementAgent(
-    x= 12.0,
-    y= 1.0,
-    angle=0,            
+    x=0.0,
+    y=1.0,
+    angle=0.0,
     radius=0.3,
     epsilon=0.1,
     box_length=3,
     box_width=1.5,
-    target=np.array([-1, 1]),
     dt=0.1
 )
 
 agent3 = MovementAgent(
-    x= 11.5,
-    y= 1.0,
-    angle=0,            
+    x=0.0,
+    y=-1.0,
+    angle=0.0,
     radius=0.3,
     epsilon=0.1,
-    box_length=3,
+    box_length=2,
     box_width=1.5,
-    target=np.array([-1, 1]),
     dt=0.1
 )
 
 agent4 = MovementAgent(
-    x= 11.5,
-    y= 1.2,
-    angle=0,            
+    x=0.0,
+    y=-3,
+    angle=0.0,
     radius=0.3,
     epsilon=0.1,
     box_length=3,
     box_width=1.5,
-    target=np.array([-1, 1]),
     dt=0.1
 )
 
-agents = [agent1, agent2, agent3, agent4]
+agents = [agent1 ,agent2, agent3, agent4]
 
-# start-hastighet
+# start vel
 for ag in agents:
     ag.v = 0.1
 
 # --- world → screen mapping ---
-# vi zoomar in runt -3..3 i både x och y
 world_min_x, world_max_x = -10.0, 20.0
 world_min_y, world_max_y = -10.0, 10.0
 
@@ -92,15 +84,24 @@ def world_radius_to_pixels(r):
 
 def draw_scene():
     canvas.delete("all")
+    # draw ticket check point (radius 1)
+    tx, ty = world_to_screen(agent1.ticket_check_pos[0], agent1.ticket_check_pos[1])
+    ticket_r = world_radius_to_pixels(1)
 
-    # rita väggar
+    canvas.create_oval(
+        tx - ticket_r, ty - ticket_r,
+        tx + ticket_r, ty + ticket_r,
+        outline="green", width=2
+        )
+
+    # draw walls
     for w in walls:
         (x1, y1), (x2, y2) = w
         sx1, sy1 = world_to_screen(x1, y1)
         sx2, sy2 = world_to_screen(x2, y2)
         canvas.create_line(sx1, sy1, sx2, sy2, fill="black", width=3)
 
-    # rita targets (röda små cirklar)
+    # draw targets
     for ag in agents:
         ax, ay = world_to_screen(ag.target[0], ag.target[1])
         r_att = 4
@@ -110,7 +111,7 @@ def draw_scene():
             outline="red", width=2
         )
 
-    # rita agenter (blå cirklar)
+    # draw agents
     for i, ag in enumerate(agents):
         sx, sy = world_to_screen(ag.x, ag.y)
         r_pix = world_radius_to_pixels(ag.radius)
@@ -121,11 +122,10 @@ def draw_scene():
             fill="blue"
         )
 
-        # liten index-text så du ser vem som är vem
         canvas.create_text(sx, sy - 10, text=str(i+1), fill="black")
 
 
-T_STEPS = 300
+T_STEPS = 600
 step = 0
 
 def sim_step():
@@ -133,14 +133,12 @@ def sim_step():
     if step >= T_STEPS:
         return
 
-    # uppdatera agenter
     for ag in agents:
         ag.update(walls, agents)
 
     draw_scene()
     step += 1
     tk.after(50, sim_step)
-
 
 # start
 draw_scene()
